511ee4f35e0b345c50dd0e3aa54cc11fbd087e04
Alexander Panchenko <Alexander.Panchenko@dataart.com>
2020-02-10 17:49:20 +0300

    exchange_name & active_queue are managed by configuration server, create docker network with run.sh

--- run.bat
+++ run.bat
@@ -1,6 +1,8 @@
 echo Stop and Remove
 docker-compose -f docker_compose.yml down
+docker network rm pm-network
 echo Build
 call gradlew.bat clean assemble
 echo Run
+docker network create --attachable --subnet 172.22.0.0/24 pm-network
 docker-compose -f docker_compose.yml up --build --force-recreate -d
--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 echo Stop and Remove
 docker-compose -f docker-compose.yml down
+docker network rm pm-network
 echo Build
  ./gradlew clean assemble
 docker-compose -f docker-compose.yml build --force-rm
 echo Run
+docker network create --attachable --subnet 172.22.0.0/24 pm-network
 docker-compose -f docker-compose.yml up -d
